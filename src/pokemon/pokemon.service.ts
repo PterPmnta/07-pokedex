e8de132c51c2f655a0c94fdff179d3ab8bb42a3f
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-07-30 16:37:51 -0500

    changes

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-    create(createPokemonDto: CreatePokemonDto) {
-        createPokemonDto.name = createPokemonDto.name.toLowerCase();
-        return `This action adds a new pokemon`;
+    constructor(
+        @InjectModel(Pokemon.name)
+        private readonly pokemonModel: Model<Pokemon>,
+    ) {}
+
+    async create(createPokemonDto: CreatePokemonDto) {
+        try {
+            createPokemonDto.name = createPokemonDto.name.toLowerCase();
+            const pokemon = await this.pokemonModel.create(createPokemonDto);
+            return {
+                data: pokemon,
+                message: 'Pokemon created successfully',
+            };
+        } catch (error) {
+            throw new Error(error);
+        }
     }
 
     findAll() {
