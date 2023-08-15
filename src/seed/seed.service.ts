5066608e3580eb8696a8f42f1766483c5f186310
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-08-14 21:51:18 -0500

    clase 92 - realizar peticion htt desde nest

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@nestjs/common';
 
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
+
+export interface PokeapiResponse {
+    count: number;
+    next: string;
+    previous: null;
+    results: Result[];
+}
+
+export interface Result {
+    name: string;
+    url: string;
+}
+
 @Injectable()
 export class SeedService {
-    executeSeed() {
-        return `This action returns all seed`;
+    constructor(private readonly httpService: HttpService) {}
+
+    async executeSeed() {
+        const { data } = await firstValueFrom(
+            this.httpService
+                .get<PokeapiResponse>(
+                    'https://pokeapi.co/api/v2/pokemon?limit=10',
+                )
+                .pipe(
+                    catchError((error: AxiosError) => {
+                        console.error(error.response.data);
+                        throw error.response.data;
+                    }),
+                ),
+        );
+
+        data.results.forEach(({ name, url }) => {
+            const segments = url.split('/');
+            const nro: number = +segments[segments.length - 2];
+        });
+
+        return data.results;
     }
 }
